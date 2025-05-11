08906f44f86562069efb946f0bcc412993eb6255
TysonPWilliams <tyson.williams95@gmail.com>
2025-05-11 21:51:52 +1000

    Added dotenv to dependencies; added security and error handling middleware; added bcrypt, cors, and helemt to dependencies

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+import 'dotenv/config' // Load environment variables from .env file
 import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
 import { connect } from './db.js'
+
+// console.log(process.env) // Check if environment variables are loaded correctly
 
 const app = express()
 const port = 8080
 
+app.use(helmet());
+app.use(cors());
+
+app.use(express.json());
+
+
+// TODO: Add your routes to use here
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    res.status(err.status).send({error: err.message})
+});
 
 
 app.listen(port, async () => {
     console.log(`Back-end is listening on port ${port}`)
     connect()
-})
+});
