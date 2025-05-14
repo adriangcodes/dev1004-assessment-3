0198c2cdc8e318e7f1501c50e59ff3fcf0c95491
Adrian Gidaro <adriangidaro@gmail.com>
2025-05-14 19:21:14 +1000

    #32 Created wallet model

---
+++ models/wallet.js
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose'
+import User from './user.js'
+import Cryptocurrency from './cryptocurrency.js'
+
+const walletSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: User
+    },
+    cryptoType: {
+        type: Schema.Types.ObjectId,
+        ref: Cryptocurrency
+    },
+    amount: {
+        type: Number,
+        min: [0, 'Wallet cannot have a negative amount of coins.'],
+        // Max limit set based on initial operation of Bitcoin only
+        max: [21000000, 'Amount cannot exceed 21 million.'],
+        validate: {
+        validator: function (v) {
+            return /^\d+(\.\d{1,8})?$/.test(v.toString())
+        },
+        message: 'Amount must be a number with up to 8 decimal places'
+        }
+    }
+}, { timestamps: true })
+
+const Wallet = model('Wallet', walletSchema)
+
+export default Wallet
