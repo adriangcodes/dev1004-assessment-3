e7b7e11aed73ef9fdd58f5e420709e73fa743c34
Adrian Gidaro <adriangidaro@gmail.com>
2025-05-19 19:00:39 +1000

    Typo and layout fixes

--- routes/loan_request_routes.js
+++ routes/loan_request_routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { auth } from "../auth.js"; // Import the JWT middleware
+import { auth, adminOnly } from "../auth.js"; // Import the JWT middleware
 import LoanRequest from "../models/loan_request.js";
 import Cryptocurrency from "../models/cryptocurrency.js";
 import InterestTerm from "../models/interest_term.js";
@@ -7,37 +7,32 @@ import User from "../models/user.js";
 
 const router = Router();
 
-// Create a new loan request
+// Create a new loan request (authorised user only)
 router.post("/loan-request", auth, async (req, res) => {
-    
     try {
-        
-        const userId = req.auth.id; // Get the user ID from the authenticated user 
-
+        // Get the user ID from the authenticated user 
+        const userId = req.auth.id;
         // Extract loan request details from req.body
         const { request_amount, loan_term, cryptocurrency_symbol} = req.body;
-
+        // Validate cryptocurrency
         const cryptoDoc = await Cryptocurrency.findOne({ symbol: cryptocurrency_symbol})
         if (!cryptoDoc) {
             return res.status(400).json({ error: `Cryptocurrency ${cryptocurrency_symbol} not found`})
         }
-
+        // Validate interest term
         const interestTermDoc = await InterestTerm.findOne({loan_length: loan_term})
-        
         if (!interestTermDoc) {
             return res.status(400).json({ error: `Loan term of ${loan_term} months not found. Try 1, 3, or 6 months`})
         }
-
         // Add new Loan Request to the db
         const loanRequest = await LoanRequest.create({ 
             borrower_id: userId,
             request_amount,
             interest_term: interestTermDoc._id,
             cryptocurrency: cryptoDoc._id,
-        });
-        
+        })
+        // Return success message to client
         res.status(201).json({ message: "Loan request created successfully", loanRequest });
-    
     }
     catch (error) {
         console.error("Error creating loan request:", error);
@@ -48,7 +43,7 @@ router.post("/loan-request", auth, async (req, res) => {
     }
 })
 
-// Get all loan requests
+// Get all loan requests (authorised user only)
 router.get('/loan-request', auth, async (req, res) => {
     try {
         // Get all loan requests from the database
@@ -65,12 +60,11 @@ router.get('/loan-request', auth, async (req, res) => {
     }
 })
 
-// Get a specific loan request by ID
+// Get a specific loan request by ID (authorised user only)
 router.get('/loan-request/:id', auth, async (req, res) => {
     try {
         // Get the loan request ID from the request parameters
         const loanRequestId = req.params.id;
-
         // Find the loan request by ID and populate necessary fields
         const loanRequest = await LoanRequest
             .findById(loanRequestId)
@@ -78,20 +72,19 @@ router.get('/loan-request/:id', auth, async (req, res) => {
             .populate('borrower_id', 'name email') // Populate borrower details
             .populate('interest_term', 'loan_length interest_rate') // Populate interest term details
             .populate('cryptocurrency', 'symbol name'); // Populate cryptocurrency details
-
+        // Force error if loan request is not found
         if (!loanRequest) {
             return res.status(404).json({ error: "Loan request not found" });
         }
-
+        // Return response to client
         res.status(200).json(loanRequest);
-
     } catch (error) {
         console.error("Error fetching loan request:", error);
         res.status(500).send({ error: "Internal server error" });
     }
 })
 
-// Fund a new loan
+// Fund a new loan (authorised user only)
 router.post('/fund-loan', auth, async (req, res) => {
     try {
         const funderId = req.auth.id; // Get the funder ID from the authenticated user
