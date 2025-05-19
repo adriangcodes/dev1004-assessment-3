3831af5d22169f4a86d915dbbfb14e3898b79579
Adrian Gidaro <adriangidaro@gmail.com>
2025-05-19 19:06:32 +1000

    Updated loan_request_routes
    - Added admin-only update and delete routes
    - Moved fund loan request code to loan_request_controller.js (WIP)

--- routes/loan_request_routes.js
+++ routes/loan_request_routes.js
@@ -3,7 +3,6 @@ import { auth, adminOnly } from "../auth.js"; // Import the JWT middleware
 import LoanRequest from "../models/loan_request.js";
 import Cryptocurrency from "../models/cryptocurrency.js";
 import InterestTerm from "../models/interest_term.js";
-import User from "../models/user.js";
 
 const router = Router();
 
@@ -84,61 +83,42 @@ router.get('/loan-request/:id', auth, async (req, res) => {
     }
 })
 
-// Fund a new loan (authorised user only)
-router.post('/fund-loan', auth, async (req, res) => {
+// Update loan request (admin only)
+async function update(req, res) {
     try {
-        const funderId = req.auth.id; // Get the funder ID from the authenticated user
-        
-        //  Extract loan request details from req.body
-        const { loan_request_id, funding_amount } = req.body;
-
-        // Find the loan request by ID
-        const loanRequest = await LoanRequest
-            .findById(loan_request_id)
-            .populate('cryptocurrency', 'symbol');
+        const loanRequestId = req.params.id
+        // Find crypto by id
+        const loanRequest = await LoanRequest.findById(loanRequestId)
+        // Return error if id does not exist
         if (!loanRequest) {
-            return res.status(404).json({ error: "Loan request not found" });
-
-        } else if (loanRequest.status !== 'active') {
-            return res.status(400).json({ error: "Loan request is not active" });
+            return res.status(404).send({ error: `Loan request with id ${loanRequestId} not found.` })
         }
-
-        // Get funder user
-        const funder = await User.findById(funderId);
-        if (!funder) {
-            return res.status(404).json({ error: "Funder not found" });
-        }
-
-        const cryptoSymbol = loanRequest.cryptocurrency.symbol;
-        console.log(cryptoSymbol);
-
-        const currentBalance = funder.wallet?.get(cryptoSymbol) ?? 0;
-
-        if (currentBalance < funding_amount) {
-            return res.status(400).json({ error: `Insufficient balance in ${cryptoSymbol} wallet` });
-        }
-
-        // Deduct the funding amount from the funder's wallet
-        funder.wallet.set(cryptoSymbol, currentBalance - funding_amount);
-        await funder.save();
-
-        // Update the loan request with the funding amount
-        loanRequest.funding_amount = funding_amount;
-        loanRequest.status = "funded";
-        await loanRequest.save();
-
-        res.status(200).json({ message: "Loan request funded successfully", loanRequest });
+        // Perform the update
+        const updatedLoanRequest = await LoanRequest.findByIdAndUpdate(loanRequestId, req.body, { returnDocument: 'after' })
+        // Send response to client
+        res.status(200).send(updatedLoanRequest)
+    } catch (err) {
+        res.status(400).send({ error: err.message })
     }
-    catch (error) {
-        console.error("Error funding loan request:", error);
-        if (error.name === 'ValidationError') {
-            return res.status(400).json({ error: error.message });
+}
+
+router.put('/loan-request/:id', auth, adminOnly, update)
+router.patch('/loan-request/:id', auth, adminOnly, update)
+
+// Delete loan request (admin only)
+router.delete('/loan-request/:id', auth, adminOnly, async (req, res) => {
+    try {
+        const loanRequestId = req.params.id
+        const loanRequest = await LoanRequest.findByIdAndDelete(loanRequestId)
+        if (crypto) {
+            res.status(200).send({ message: 'Loan request deleted successfully.' })
+        } else {
+            res.status(404).send({ error: `Loan request with id ${loanRequestId} not found.` })
         }
-        return res.status(500).json({ error: "Internal server error" });
+    } catch (err) {
+        res.status(400).send({ error: err.message })
     }
 })
-
-
 
 
 export default router;
