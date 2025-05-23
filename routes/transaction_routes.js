fef19f46c4d2a89042181d2aefd6025837ed193e
Adrian Gidaro <adriangidaro@gmail.com>
2025-05-23 11:02:26 +1000

    Updated transaction get route based on front-end testing

--- routes/transaction_routes.js
+++ routes/transaction_routes.js
@@ -23,24 +23,44 @@ router.get('/transactions', auth, adminOnly, async (req, res) => {
     }
 })
 
-// Get all transaction from a single user (authorised user only)
-router.get('/transactions/:id', auth, async (req, res) => {
-    const transactionId = req.params.id
-    // Fetch the transaction with the given id
+// // Get all transaction from a single user (authorised user only)
+// router.get('/transactions/:id', auth, async (req, res) => {
+//     const transactionId = req.params.id
+//     // Fetch the transaction with the given id
+//     try {
+//         const transaction = await Transaction.findById(transactionId)
+//     // Check the transaction exists, if not return error message
+//     if (!transaction) {
+//         return res.status(404).send({ error: `Transaction with id ${transactionId} not found.` })
+//     }
+//     // Check the user is accessing their own transaction data, if not return error message
+//     if (transaction.userId.toString() !== req.user._id.toString()) {
+//         return res.status(403).send({ error: 'Access denied: you are not authorised to view these transactions.' })
+//     }
+//     // Send transaction to client if it satisfies the above criteria
+//     res.send(transaction)
+//     } catch (err) {
+//     res.status(500).send({ error: err.message })
+//     }
+// })
+
+// Get all transactions for a user (authorised user only)
+router.get('/transactions/user/:userId', auth, async (req, res) => {
+    const userId = req.params.userId
+    // Check if the user is trying to access their own transactions
+    if (userId !== req.auth.id) {
+        return res.status(403).send({ error: 'Access denied: you can only view your own transactions.' })
+    }
     try {
-        const transaction = await Transaction.findById(transactionId)
-    // Check the transaction exists, if not return error message
-    if (!transaction) {
-        return res.status(404).send({ error: `Transaction with id ${transactionId} not found.` })
-    }
-    // Check the user is accessing their own transaction data, if not return error message
-    if (transaction.userId.toString() !== req.user._id.toString()) {
-        return res.status(403).send({ error: 'Access denied: you are not authorised to view these transactions.' })
-    }
-    // Send transaction to client if it satisfies the above criteria
-    res.send(transaction)
+        const transactions = await Transaction.find({
+            $or: [{ fromUser: userId }, { toUser: userId }]
+        }).populate('fromUser toUser fromWallet toWallet')
+        if (!transactions || transactions.length == 0) {
+            return res.status(404).send({message: "No transactions found"})
+        }
+        res.send(transactions)
     } catch (err) {
-    res.status(500).send({ error: err.message })
+        res.status(500).send({ error: err.message })
     }
 })
 
